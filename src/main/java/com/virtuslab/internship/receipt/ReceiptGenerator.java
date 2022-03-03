8c0c0527d0b022bf25f70dd014cb403d5a19f534
JanickiJ <kubajanicki244@gmail.com>
2022-03-03 15:12:18 +0100

    fixed problems that caused test failure

--- src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
+++ src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
@@ -1,19 +1,28 @@
 package com.virtuslab.internship.receipt;
 
 import com.virtuslab.internship.basket.Basket;
+import com.virtuslab.internship.product.Product;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ReceiptGenerator {
 
     public Receipt generate(Basket basket) {
-        var receipt = getReceipt(basket);
-        return receipt;
+        return getReceipt(basket);
     }
 
     private Receipt getReceipt(Basket basket) {
-        List<ReceiptEntry> receiptEntries = new ArrayList<>();
+        Map<Product, Integer> productQuantities = new HashMap<>();
+        basket.getProducts().forEach(product -> {
+            Integer productQuantity = productQuantities.get(product);
+            productQuantities.put(product, productQuantity == null ? 1 : productQuantity + 1);
+        });
+        List<ReceiptEntry> receiptEntries = productQuantities.entrySet().stream().map(entry ->
+                new ReceiptEntry(entry.getKey(), entry.getValue())
+        ).collect(Collectors.toList());
         return new Receipt(receiptEntries);
     }
 }
